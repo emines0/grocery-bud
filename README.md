2c945cee372a068496daec4e8b9cc1df4c210b1f
emines0 <emin.sefa1@gmail.com>
2023-02-11 22:18:36 +0100

    structure

---
+++ README.md
@@ -0,0 +1,4 @@
+# Grocery bud
+
+- useState
+-
--- src/App.js
+++ src/App.js
@@ -3,7 +3,47 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('')
+  const [list, setList] = useState([])
+  const [isEditing, setIsEditing] = useState(false)
+  const [editID, setEditID] = useState(null)
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('hello')
+  }
+  return (
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}></form>
+      {alert.show && <Alert />}
+      {/*
+       * If alert.show is true display Alert component
+       */}
+      <h3>grocery bud</h3>
+      <div className='form-control'>
+        <input
+          type='text'
+          className='grocery'
+          placeholder='eg. eggs'
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value)
+          }}
+          /*
+           * On change set name to e.target.value (what user type in the input field)
+           */
+        />
+        <button type='submit' className='submit-btn'>
+          {isEditing ? 'edit' : 'submit'}
+        </button>
+      </div>
+      <div className='grocery-container'>
+        <List />
+        <button className='clear-btn'>clear items</button>
+      </div>
+    </section>
+  )
 }
 
 export default App
